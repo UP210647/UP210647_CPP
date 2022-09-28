8f754caff42738923d3612ceaf6e089a5bdaa258
UP210647 <up210647@alumnos.upa.edu.mx>
2022-09-28 09:16:35 -0500

    x

--- U1/edades.cpp
+++ U1/edades.cpp
@@ -4,42 +4,42 @@ Fecha: 16/09/2022
 Descripcion: Se indica etapa de vida segun la edad del ususarios
 */
 
-#include<iostream>
-#include<stdio.h>
+#include <iostream>
+#include <stdio.h>
 
 using namespace std;
-
 
 int main()
 {
     int edad;
 
-    printf ("Dame una edad entre 1 a 150 \n");
-    scanf ("%d", &edad );
+    printf("Dame una edad entre 1 a 150 \n");
+    scanf("%d", &edad);
 
-    if (edad>=1 && edad<=150){
-        if (edad>=1 && edad<=30){
+    if (edad >= 1 && edad <= 150){
+        if (edad >= 1 && edad <= 30){
 
             printf("Primera edad");
+        }
+        else if (edad >= 31 && edad <= 60){
 
-        }else{ if(edad>=31 && edad<=60){
+            printf("Segunda edad");
+        }
+        else if (edad >= 61 && edad <= 60)
+        {
+            printf("Tercera edad");
+        }
+        else if (edad>=61 && edad<=150){
 
-                printf("Segunda edad");
-
-            }else{ if (edad>=61 && edad<=60){
-
-                  printf("Tercera edad");
-
-            }else{
-
-                   printf("Horas extras");
-                   
-            }
+            printf("Horas extras");
+        }
+    
+        
     }
-    }
-    }else{
-        printf ("Numero invalido");
+    else{
+
+    printf("Numero invalido");
     }
 
-    return 0;
+return 0;
 }
